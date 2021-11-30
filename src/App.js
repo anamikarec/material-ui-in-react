7d7c4e2c93f9e96d3948c774e3600e85c5f939a1
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2021-11-30 23:39:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./styles.css";
+import Button from "@mui/material/Button";
+import styled from "styled-components";
+
+const Div = styled.div`
+  margin: 10px;
+  padding: 1rem;
+  display: flex;
+  background: yellow;
+  gap: 0.5rem;
+  flex-direction: column;
+`;
+
+export default function App() {
+  return (
+    <div className="App">
+      <Div>
+        <div>
+          <Button variant="contained">Hello World1</Button>
+        </div>
+
+        <div>
+          <Button variant="outlined">Hello World2</Button>
+        </div>
+        <div>
+          <Button variant="text">Hello World3</Button>
+        </div>
+      </Div>
+    </div>
+  );
+}
